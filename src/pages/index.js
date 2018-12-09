97d449b99571eac78fbc2ba24585346be8f9acfc
mehamasum <mehamasum@gmail.com>
2018-12-10 02:51:37 +0600

    add project card css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
-import Helmet from 'react-helmet'
-
-import Bio from '../components/Bio'
-import Tag from '../components/Tag'
 import Layout from '../components/Layout'
-import Pagination from '../components/Pagination'
+import { Grid, Row, Col } from 'react-bootstrap'
+import '../assets/bootstrap.min.css'
+import '../assets/site.css'
 import { rhythm } from '../utils/typography'
 
 class Index extends React.Component {
@@ -15,18 +13,45 @@ class Index extends React.Component {
     console.log(data)
     const projects = data.allProjectsJson.edges
     return (
-      <Layout
-        location={this.props.location}
-        title={blogSlogan}
-        subTitle={blogTitle}
-      >
-        <Link to={'/blog'}>Blog</Link>
-        <div>
+      <Grid>
+        <Row>
           {projects.map(({ node: project }) => {
-            return <div key={project.slug}>{project.name}</div>
+            return (
+              <Col xs={12} sm={6} md={4} lg={3} key={project.id}>
+                <div className="card">
+                  <aside className="entry-badge">
+                    <img
+                      alt="Winner"
+                      className="winner"
+                      src="https://devpost-challengepost.netdna-ssl.com/assets/shared/software/winner-ribbon-d6a3513950ca29607c8d8682f419dd99.png"
+                    />
+                  </aside>
+                  <img
+                    src="https://picsum.photos/400/300/?random"
+                    alt="Avatar"
+                    style={{ width: '100%' }}
+                  />
+                  <div className="card-body">
+                    <h2
+                      style={{
+                        marginTop: 0,
+                      }}
+                    >
+                      {project.name}
+                    </h2>
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry. Lorem Ipsum has been the industry's
+                      standard dummy text ever since the 1500s.
+                    </p>
+                    <a href="#">Website</a> &bull; <a href="#">Github</a>
+                  </div>
+                </div>
+              </Col>
+            )
           })}
-        </div>
-      </Layout>
+        </Row>
+      </Grid>
     )
   }
 }
